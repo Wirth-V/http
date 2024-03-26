948ff1ec96046de8758bc021cb9197659db97ff7
Virth <virth@example.com>
2024-03-26 11:58:58 +0300

    sanitize

--- net_http/moduls/Sanitize.go
+++ net_http/moduls/Sanitize.go
@@ -1,13 +1,18 @@
 package moduls
 
-import "regexp"
+import (
+	"os"
+	"regexp"
+)
 
 func Sanitize(s string) {
 	if len(s) >= 9 {
 		ErrorLog.Println("Превышен максимальный размер вхожных данных (не более 8 символов)")
+		os.Exit(1)
 	}
 	if b, _ := regexp.MatchString("[^a-zA-Z0-9]+", s); b {
 		ErrorLog.Println("Присутсвуют недопустимые символы")
+		os.Exit(1)
 	}
 
 }
