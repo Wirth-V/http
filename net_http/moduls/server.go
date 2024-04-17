176f37b47bed42432e683ae7085e4f22215473ad
Virth <virth@example.com>
2024-04-17 03:57:47 +0300

    update ogic

--- net_http/moduls/server.go
+++ net_http/moduls/server.go
@@ -286,7 +286,6 @@ func GenerateID() string {
 func db_control(connString string, Table string) error {
 	//разбора строки, возвращает структуру
 	connConfig, err := pgx.ParseConfig(connString)
-	fmt.Println(connConfig)
 	//pgx.ConnConfig, содержащую параметры соединения
 	if err != nil {
 		return err
@@ -297,7 +296,6 @@ func db_control(connString string, Table string) error {
 
 	//меняет название подключаемой бд
 	connConfig.Database = "postgres"
-	fmt.Println(connConfig)
 	//Соединение с бд, чтобы проверить ее существование
 	conn_db, err := pgx.ConnectConfig(context.Background(), connConfig)
 	if err != nil {
@@ -363,22 +361,5 @@ func db_control(connString string, Table string) error {
 		}
 
 	}
-
-	/*
-		var exists_table bool
-		err = conn.QueryRow(context.Background(), "SELECT EXISTS (SELECT 1 FROM information_schema.tables WHERE table_name = $1)", Table).Scan(&exists_table)
-		if err != nil {
-			return err
-		}
-
-		if !exists_table {
-			// Создание таблицы
-			_, err := conn.Exec(context.Background(), "CREATE TABLE "+Table+" (id VARCHAR(8), name VARCHAR(30))")
-			if err != nil {
-				fmt.Println("Error creating table:", err)
-				return err
-			}
-		}
-	*/
 	return nil
 }
